1c9a072c73163b92c4495df862058884527cdcfe
damocles99 <your.email@example.com>
2026-09-07 11:17:33 -0500

    configuracion de astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zenix.farrosfr.com",
+  site: "https://importadorasucre.ec/",
   base: "/",
   trailingSlash: "always",
 
